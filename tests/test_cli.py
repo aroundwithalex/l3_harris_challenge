546382cb3dca24daf7cb630d0ac8b81d5958d7f0
Alex Pope <129560535+popealex@users.noreply.github.com>
2023-05-24 10:42:12 +0100

    Add unit tests for exception handling

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,11 +8,36 @@ Typical Usage
     $ pytest -v
 """
 
+import os
+from tempfile import TemporaryDirectory
+
+import pytest
+
 from click.testing import CliRunner
 from src.cli import closest
-from src.search import Searcher
 
-def test_closest_command():
+@pytest.fixture
+def setup():
+    """
+    Sets up the test environment
+
+    Creates the CliRunner object so that it can be used
+    in later tests:
+
+    Args:
+        None
+    
+    Returns:
+        None
+    
+    Raises:
+        None
+    """
+
+    runner = CliRunner()
+    return runner
+
+def test_closest_command(setup):
     """
     Tests the closest CLI command
 
@@ -29,7 +54,51 @@ def test_closest_command():
         None
     """
 
-    runner = CliRunner()
-    result = runner.invoke(closest, ["--lat", "52.772078", "--long", "-1.664884"])
+    result = setup.invoke(closest, ["--lat", "52.772078", "--long", "-1.664884"])
     assert "Closest Airport" in result.output
     assert result.exit_code == 0
+
+def test_file_not_found_failure(setup):
+    """
+    Tests what happens if no co-ordinate file exists
+
+    This test ensures that the CLI fails if no co-ordinate file is found
+    in the expected place. 
+
+    Args:
+        setup -> pytest fixture
+    
+    Returns:
+        None
+    
+    Raises:
+        None
+    """
+
+    current_dir = os.getcwd()
+    with TemporaryDirectory() as tempdir:
+        os.chdir(tempdir)
+        result = setup.invoke(closest, ["--lat", "52.772078", "--long", "-1.664884"])
+        os.chdir(current_dir)
+        assert "No such file" in result.output
+
+def test_value_error_failure(setup):
+    """
+    Tests what happens if bad co-ordinates are passed
+
+    This tests checks what happens when bad co-ordinates are passed to the CLI. IT
+    ensures that an error is printed to the screen.
+    
+    Args:
+        setup -> pytest fixture
+    
+    Returns:
+        None
+    
+    Raises:
+        None
+    """
+
+    result = setup.invoke(closest, ["--lat", "1000", "--long", "-10000"])
+    assert "Error" in result.output
+
